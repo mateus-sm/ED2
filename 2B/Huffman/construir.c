3e1b9555480e938305cb687f0a4d8cfd066527ee
Mateus Maciel <matteus6537@hotmail.com>
2025-05-12 10:16:12 -0300

    UPDATE

--- 2B/Huffman/construir.c
+++ 2B/Huffman/construir.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 
 struct listaRegistros {
     int simbolo;
@@ -13,6 +14,15 @@ struct listaRegistros {
 };
 
 typedef struct listaRegistros ListR;
+
+int getBits(int n) {
+    //log de n(elementos diferentes em palavra)
+    int i = 1;
+    for(; n > pow(2,i);){
+        i++;
+    }
+    return i;
+}
 
 bool palavraCadastrada(char *palavra, ListR *lista) {
     bool flag = false;
@@ -49,39 +59,101 @@ int contaFrequencia(char *palavra, char *frase) {
     return palavras;
 }
 
+ListR *criaNoListaReg(int simbolo, char *palavra, int freq, int cod) {
+    ListR *novo = (ListR*)malloc(sizeof(ListR));
 
-void construirListaRegistros(ListR **lista) {
-    char *frase = "amar e sonhar sonhar e viver viver e curtir curtir e amar"; //printf("%s\n", frase);
+    novo->simbolo = simbolo;
+    strcpy(novo->palavra, palavra);
+    novo->freq = freq;
+    novo->cod = cod;
+    novo->prox = NULL;
+
+    return novo;
+}
+
+void inserir(ListR **lista, int simbolo, char *palavra, int freq, int cod) {
+    ListR *ant = NULL, *atual = *lista;
+    ListR *no = criaNoListaReg(simbolo, palavra, freq, cod);
+
+    if (*lista == NULL) {
+        *lista = no;
+    } else {
+        // 1) percorre até achar o ponto de inserção
+        while (atual != NULL && atual->freq < freq) {
+            ant = atual;
+            atual = atual->prox;
+        }
+
+        // 2) insere no início se ant == NULL
+        if (ant == NULL) {
+            no->prox = *lista;
+            *lista   = no;
+        }
+        // 3) ou insere entre ant e atual
+        else {
+            ant->prox = no;
+            no->prox  = atual;
+        }
+    }
+}
+
+void exibir(ListR *lista) {
+    ListR *atual = lista;
+    while (atual != NULL) {
+        printf("\"%s\" ", atual->palavra);
+        printf("Simbolo: %d\n", atual->simbolo);
+        printf("Frequencia: %d ", atual->freq);
+        printf("Codigo: %d\n\n", atual->cod);
+        atual = atual->prox;
+    }
+}
+
+void construirListaRegistros(ListR **lista, char *frase) {
     char palavra[20];
-    int freq = 0, simbolo = 0;
+    int freq = 0, simbolo = 0, espacos = 0;
     ListR *lr = NULL;
 
     palavra[0] = '\0';
     for (int i = 0, k = 0; i <= strlen(frase); i++) {
-        if (frase[i] == ' ' && palavra[0] != '\0') { //Atingi o Delimitador
-            palavra[k++] = '\0'; //printf("palavra = %s\n", palavra);
-
-            if (!palavraCadastrada(palavra, *lista)) {
-                freq = contaFrequencia(palavra, frase);
-                //printf("%s tem freq = %d\n", palavra, freq);
-
-                lr = (ListR*)malloc(sizeof(ListR));
-            }
-
-            palavra[0] = '\0';
-            k = 0;
+        if (frase[i] == ' ') {
+            espacos++;
             i++;
+
+            if (palavra[0] != '\0') { //Atingi o Delimitador e a palavra nao esta vazia
+                palavra[k++] = '\0'; 
+                //printf("palavra = %s\n", palavra);
+
+                if (!palavraCadastrada(palavra, *lista)) {
+                    freq = contaFrequencia(palavra, frase);
+                    //printf("%s tem freq = %d\n", palavra, freq);
+
+                    inserir(lista, simbolo++, palavra, freq, 0);
+                }
+
+                palavra[0] = '\0';
+                k = 0;
+            }
         }
         
         palavra[k++] = frase[i];
     }
+
+    if (espacos > 0) {
+        inserir(lista, simbolo++, " ", espacos, 0);
+    }
 }
 
 int main(void) {
-    ListR *lista = NULL;
+    ListR *lista = NULL, *aux = NULL;
+    char x;
+    char *frase = " amar e sonhar sonhar e viver viver e curtir curtir e amar cada um tera "
+                  "uma escolha cada um fara a escolha cada um escolhe a sua escolha levaremos "
+                  "um tempo para crescer levaremos um tempo para amadurecer levaremos um tempo "
+                  "para entender levaremos um tempo para envelhecer levaremos um tempo para morrer"; //printf("%s\n", frase);
 
-    construirListaRegistros(&lista);
+    construirListaRegistros(&lista, frase);
 
+    exibir(lista);
 
     system("pause");
     return 0;
