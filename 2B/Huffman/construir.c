a1b05babc127df6353092764cdc01e230e159dd1
Mateus Maciel <matteus6537@hotmail.com>
2025-05-13 08:39:05 -0300

    UPDATE

--- 2B/Huffman/construir.c
+++ 2B/Huffman/construir.c
@@ -15,10 +15,48 @@ struct listaRegistros {
 
 typedef struct listaRegistros ListR;
 
+struct tree {
+    struct tree *esq;
+    int      simbolo;
+    int   frequencia;
+    struct tree *dir;
+};
+
+typedef struct tree Tree;
+
+//Struct ListR
+ListR *criaNoListaReg(int simbolo, char *palavra, int freq, int cod);
+void inserirListR(ListR **lista, int simbolo, char *palavra, int freq, int cod);
+void exibirListR(ListR *lista);
+
+//lista de registros
+void construirListaRegistros(ListR **lista, char *frase);
+bool palavraCadastrada(char *palavra, ListR *lista);
+int contaFrequencia(char *palavra, char *frase);
+
+int getBits(int n);
+
+int main(void) {
+    ListR *lista = NULL;
+
+    char *frase1 = " o tempo perguntou pro tempo quanto tempo o tempo tem "
+                  "que tempo o tempo respondeu pro tempo ";
+    //printf("%s\n", frase1);
+    char *frase2 = " o tempo perguntou pro tempo quanto tempo o tempo tem "
+                  "o tempo respondeu pro tempo que o tempo o tempo tem ";
+    //printf("%s\n", frase2);
+
+    construirListaRegistros(&lista, frase2);
+    exibirListR(lista);
+
+    system("pause");
+    return 0;
+}
+
 int getBits(int n) {
     //log de n(elementos diferentes em palavra)
     int i = 1;
-    for(; n > pow(2,i);){
+    for(; n > pow(2,i);) {
         i++;
     }
     return i;
@@ -38,22 +76,23 @@ bool palavraCadastrada(char *palavra, ListR *lista) {
 }
 
 int contaFrequencia(char *palavra, char *frase) {
-    int count = 0, palavras = 0;
+    int palavras = 0, len = strlen(frase);
     char palavraLida[50];
 
-    for(int i = 0, k = 0; i <= strlen(frase); i++) {
-        if ((frase[i] == ' ' || frase[i] == '\0') && palavraLida[0] != '\0') { //Delimitador
-            palavraLida[k++] = '\0'; //printf("palavra lida = %s\n", palavraLida);
+    for (int i = 0, k = 0; i <= len; i++) {
+        if (frase[i] == ' ' || frase[i] == '\0') { //Delimitador
+            if (k > 0) {
+                palavraLida[k++] = '\0'; //printf("palavra lida = %s\n", palavraLida); system("pause");
 
-            if (strcmp(palavraLida, palavra) == 0) {
-                palavras++;
+                if (strcmp(palavraLida, palavra) == 0) {
+                    palavras++;
+                }
+                
+                k = 0;
             }
-            
-            palavraLida[0] = '\0';
-            k = 0;
-            i++;
+        } else {
+            palavraLida[k++] = frase[i];
         }
-        palavraLida[k++] = frase[i];
     }
 
     return palavras;
@@ -71,34 +110,31 @@ ListR *criaNoListaReg(int simbolo, char *palavra, int freq, int cod) {
     return novo;
 }
 
-void inserir(ListR **lista, int simbolo, char *palavra, int freq, int cod) {
+void inserirListR(ListR **lista, int simbolo, char *palavra, int freq, int cod) {
     ListR *ant = NULL, *atual = *lista;
     ListR *no = criaNoListaReg(simbolo, palavra, freq, cod);
 
     if (*lista == NULL) {
         *lista = no;
     } else {
-        // 1) percorre até achar o ponto de inserção
         while (atual != NULL && atual->freq < freq) {
             ant = atual;
             atual = atual->prox;
         }
 
-        // 2) insere no início se ant == NULL
         if (ant == NULL) {
             no->prox = *lista;
             *lista   = no;
-        }
-        // 3) ou insere entre ant e atual
-        else {
+        } else {
             ant->prox = no;
             no->prox  = atual;
         }
     }
 }
 
-void exibir(ListR *lista) {
+void exibirListR(ListR *lista) {
     ListR *atual = lista;
+    printf("Lista de Registros: \n");
     while (atual != NULL) {
         printf("\"%s\" ", atual->palavra);
         printf("Simbolo: %d\n", atual->simbolo);
@@ -111,50 +147,29 @@ void exibir(ListR *lista) {
 void construirListaRegistros(ListR **lista, char *frase) {
     char palavra[20];
     int freq = 0, simbolo = 0, espacos = 0;
-    ListR *lr = NULL;
 
-    palavra[0] = '\0';
     for (int i = 0, k = 0; i <= strlen(frase); i++) {
-        if (frase[i] == ' ' || frase[i] == '\0') {
+        if (frase[i] == ' ' || frase[i] == '\0') { //Atingi o Delimitador
             espacos += (frase[i] == ' ') ? 1 : 0;
-            i++;
 
-            if (palavra[0] != '\0') { //Atingi o Delimitador e a palavra nao esta vazia
-                palavra[k++] = '\0'; 
-                //printf("palavra = %s\n", palavra);
+            if (k > 0) { //Atingi o Delimitador e a palavra nao esta vazia
+                palavra[k] = '\0'; //printf("palavra = %s\n", palavra); system("pause");
 
                 if (!palavraCadastrada(palavra, *lista)) {
                     freq = contaFrequencia(palavra, frase);
                     //printf("%s tem freq = %d\n", palavra, freq);
 
-                    inserir(lista, simbolo++, palavra, freq, 0);
+                    inserirListR(lista, simbolo++, palavra, freq, 0);
                 }
 
-                palavra[0] = '\0';
                 k = 0;
             }
+        } else {
+            palavra[k++] = frase[i];
         }
-        
-        palavra[k++] = frase[i];
     }
 
     if (espacos > 0) {
-        inserir(lista, simbolo++, " ", espacos, 0);
+        inserirListR(lista, simbolo++, " ", espacos, 0);
     }
-}
-
-int main(void) {
-    ListR *lista = NULL, *aux = NULL;
-    char x;
-    char *frase = " amar e sonhar sonhar e viver viver e curtir curtir e amar cada um tera "
-                  "uma escolha cada um fara a escolha cada um escolhe a sua escolha levaremos "
-                  "um tempo para crescer levaremos um tempo para amadurecer levaremos um tempo "
-                  "para entender levaremos um tempo para envelhecer levaremos um tempo para morrer"; //printf("%s\n", frase);
-
-    construirListaRegistros(&lista, frase);
-
-    exibir(lista);
-
-    system("pause");
-    return 0;
 }
